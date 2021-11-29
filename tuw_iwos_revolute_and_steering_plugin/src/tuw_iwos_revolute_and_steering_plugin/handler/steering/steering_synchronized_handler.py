36948db13a78977107fc95e6a07de035db0b0342
Eugen <eugen.kaltenegger@gmail.com>
2021-11-29 17:46:09 +0100

    updated shebang

--- tuw_iwos_revolute_and_steering_plugin/src/tuw_iwos_revolute_and_steering_plugin/handler/steering/steering_synchronized_handler.py
+++ tuw_iwos_revolute_and_steering_plugin/src/tuw_iwos_revolute_and_steering_plugin/handler/steering/steering_synchronized_handler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from tuw_iwos_revolute_and_steering_plugin.tool.controller_tool import ControllerTool
 from tuw_iwos_revolute_and_steering_plugin.tool.synchronized_controller import SynchronizedController
