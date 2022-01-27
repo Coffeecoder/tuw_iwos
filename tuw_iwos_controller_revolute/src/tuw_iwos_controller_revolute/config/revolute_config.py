b09a4ca285193a389c67e4dca878a815410cc95d
Eugen <eugen.kaltenegger@gmail.com>
2022-01-27 13:56:05 +0100

    added reconfigure parameter

--- tuw_iwos_controller_revolute/src/tuw_iwos_controller_revolute/config/revolute_config.py
+++ tuw_iwos_controller_revolute/src/tuw_iwos_controller_revolute/config/revolute_config.py
@@ -8,13 +8,17 @@ from tuw_iwos_controller_revolute.config.config_file_reader import ConfigFileRea
 class RevoluteConfig(AbstractDefaultConfig, AbstractDynamicConfig):
 
     def __init__(self):
-        self.exchange_wheels = False
-        self.reverse_left_wheel = False
-        self.reverse_right_wheel = False
+        self.max_velocity = None
+        self.velocity_scale = None
+        self.exchange_wheels = None
+        self.reverse_left_wheel = None
+        self.reverse_right_wheel = None
 
     def from_file(self, config_file_path):
         config_content = ConfigFileReader.get_config_from_file(config_file_path=config_file_path)
 
+        self.max_velocity = config_content['Max_Velocity']
+        self.velocity_scale = config_content['Velocity_Scale']
         self.exchange_wheels = config_content['Exchange_Wheels']
         self.reverse_left_wheel = config_content['Reverse_Left_Wheel']
         self.reverse_right_wheel = config_content['Reverse_Right_Wheel']
@@ -23,12 +27,16 @@ class RevoluteConfig(AbstractDefaultConfig, AbstractDynamicConfig):
 
     def to_dynamic_reconfigure(self):
         return {
+            'max_velocity': self.max_velocity,
+            'velocity_scale': self.velocity_scale,
             'exchange_wheels': self.exchange_wheels,
             'reverse_left_wheel': self.reverse_left_wheel,
             'reverse_right_wheel': self.reverse_right_wheel
         }
 
     def from_dynamic_reconfigure(self, dynamic_config):
+        self.max_velocity = dynamic_config['max_velocity']
+        self.velocity_scale = dynamic_config['velocity_scale']
         self.exchange_wheels = dynamic_config['exchange_wheels']
         self.reverse_left_wheel = dynamic_config['reverse_left_wheel']
         self.reverse_right_wheel = dynamic_config['reverse_right_wheel']
