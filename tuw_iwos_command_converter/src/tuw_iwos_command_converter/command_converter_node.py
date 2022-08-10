0ae222263d2df9fbcf0792321fcd515adeafc326
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2022-08-10 14:33:02 +0200

    improved command converter

--- tuw_iwos_command_converter/src/tuw_iwos_command_converter/command_converter_node.py
+++ tuw_iwos_command_converter/src/tuw_iwos_command_converter/command_converter_node.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import rospy
+import tf
 
+from math import sin
+from math import pi
 from message_filters import Subscriber
 from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer
 from rospy import Publisher
@@ -18,7 +21,8 @@ class CommandConverterNode:
         self.ANGULAR_VELOCITY_THRESHOLD = 0.01
         self.ORIENTATION_THRESHOLD = 0.01
         self.ORIENTATION_CHANGING_SPEED = 0.5  # (rad/s)
-        self.WHEEL_DISPLACEMENT = 0.5
+        self.WHEEL_DISPLACEMENT: Optional[float] = None
+        self.TARGET_REACHED_DISTANCE = 0.01
 
         self.command_subscriber_topic: str = "/cmd"
         self.state_subscriber_topic: str = "/iwos_state"
@@ -32,25 +36,31 @@ class CommandConverterNode:
     def run(self):
         rospy.init_node("TUW_IWOS_COMMAND_CONVERTER")
 
+        tf_listener = tf.TransformListener()
+
+        translation: [float] = None  # [x,y,z]
+
+        while translation is None:
+            try:
+                translation, _ = tf_listener.lookupTransform('base_link', 'left_wheel_link',  rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+
+        self.WHEEL_DISPLACEMENT = 2 * translation[1]
+
         self.command_subscriber = Subscriber(self.command_subscriber_topic, TwistWithOrientation)
         self.state_subscriber = Subscriber(self.state_subscriber_topic, Joints)
         self.subscribers: List[Optional[Subscriber]] = [self.command_subscriber, self.state_subscriber]
 
-        # TODO: find appropriate value for slop
-        self.time_synchronizer = ApproximateTimeSynchronizer(self.subscribers, 10, slop=1)
+        # states are published with a frequency of 20hz and a maximum time between messages below 0.1 seconds (~0.09s)
+        self.time_synchronizer = ApproximateTimeSynchronizer(self.subscribers, 10, slop=0.1)
         self.time_synchronizer.registerCallback(self.callback)
 
         self.publisher = Publisher(name=self.publisher_topic, data_class=JointsIWS, queue_size=10)
 
-
-
-
-
         rospy.spin()
 
     def callback(self, command_message: TwistWithOrientation, state_message: Joints):
-        rospy.loginfo("match!")
-
         linear_velocity = command_message.twist.linear.x
         angular_velocity = command_message.twist.angular.z
         orientation = command_message.orientation
@@ -67,11 +77,11 @@ class CommandConverterNode:
         target_velocity_left: Optional[float] = None
         target_velocity_right: Optional[float] = None
 
-        if angular_velocity < self.ANGULAR_VELOCITY_THRESHOLD:
+        if abs(angular_velocity) < self.ANGULAR_VELOCITY_THRESHOLD:
             target_velocity_left = linear_velocity
             target_velocity_right = linear_velocity
 
-        if not angular_velocity < self.ANGULAR_VELOCITY_THRESHOLD:
+        if not abs(angular_velocity) < self.ANGULAR_VELOCITY_THRESHOLD:
             v = linear_velocity
             w = angular_velocity
             d = self.WHEEL_DISPLACEMENT
@@ -80,14 +90,22 @@ class CommandConverterNode:
             target_velocity_right: float = w * ((v / w) + d / 2.0)
 
         if abs(current_angle_left - target_angle_left) > self.ORIENTATION_THRESHOLD:
-            sign: float = float(self.signum(current_angle_left - target_angle_left))
-            change_left: float = sign * self.ORIENTATION_CHANGING_SPEED * self.WHEEL_DISPLACEMENT / 2.0
-            target_angle_left += change_left
+            left_target_distance = sin(target_angle_left) * self.WHEEL_DISPLACEMENT / 2.0
+            left_current_distance = sin(current_angle_left) * self.WHEEL_DISPLACEMENT / 2.0
+            left_distance_to_go = left_target_distance - left_current_distance
+
+            if abs(left_distance_to_go) > self.TARGET_REACHED_DISTANCE:
+                left_velocity_change = self.signum(left_distance_to_go) * 0.1
+                target_velocity_left += left_velocity_change
 
         if abs(current_angle_right - target_angle_right) > self.ORIENTATION_THRESHOLD:
-            sign: float = float(self.signum(current_angle_right - target_angle_right))
-            change_right: float = sign * self.ORIENTATION_CHANGING_SPEED * self.WHEEL_DISPLACEMENT / 2.0
-            target_velocity_right += change_right
+            right_target_distance = sin(target_angle_right + pi) * self.WHEEL_DISPLACEMENT / 2.0
+            right_current_distance = sin(current_angle_right + pi) * self.WHEEL_DISPLACEMENT / 2.0
+            right_distance_to_go = right_target_distance - right_current_distance
+
+            if abs(right_distance_to_go) > self.TARGET_REACHED_DISTANCE:
+                right_velocity_change = self.signum(right_distance_to_go) * 0.1
+                target_velocity_right += right_velocity_change
 
         output_message = JointsIWS()
         output_message.type_revolute = "cmd_velocity"
