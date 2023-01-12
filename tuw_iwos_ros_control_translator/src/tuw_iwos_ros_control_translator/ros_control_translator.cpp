62789dc7c1eadca60c7e33a02b9485e5e3461b94
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2023-01-12 16:10:24 +0100

    removed dummy package

--- tuw_iwos_ros_control_translator/src/tuw_iwos_ros_control_translator/ros_control_translator.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Eugen Kaltenegger
-
-#include <tuw_iwos_ros_control_translator/ros_control_translator.h>
-
-using RosControlTranslator;
-
-RosControlTranslator::RosControlTranslator() = default
