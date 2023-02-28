a6edcf2959f5d1c2e8edb3d7e06efafd77c278d0
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2023-02-28 16:12:46 +0100

    changing topic default

--- tuw_iwos_odometer/src/odometer_node.cpp
+++ tuw_iwos_odometer/src/odometer_node.cpp
@@ -11,7 +11,7 @@ using tuw_iwos_odometer::OdometerNode;
 OdometerNode::OdometerNode()
 {
   this->node_handle_ = ros::NodeHandle();
-  this->joint_state_subscriber_ = this->node_handle_.subscribe("joint_state", 100, &OdometerNode::update, this);
+  this->joint_state_subscriber_ = this->node_handle_.subscribe("joint_states", 100, &OdometerNode::update, this);
   this->odometer_publisher_ = this->node_handle_.advertise<nav_msgs::Odometry>("odom", 50);
   this->tf_broadcaster_ = tf::TransformBroadcaster();
 
