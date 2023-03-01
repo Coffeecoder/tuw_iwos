cff76cc670516847fdcdb827a3c1add53725a230
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2023-03-01 10:46:26 +0100

    adding naive imu odometer

---
+++ tuw_iwos_odometer/include/tuw_iwos_odometer/imu_odometer.h
@@ -0,0 +1,53 @@
+// Copyright 2023 Eugen Kaltenegger
+
+#ifndef DIP_WS_IMU_ODOMETER_H
+#define DIP_WS_IMU_ODOMETER_H
+
+#include <sensor_msgs/Imu.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <tuw_geometry/line2d.h>
+#include <tuw_geometry/point2d.h>
+#include <tuw_geometry/pose2d.h>
+
+#include <tuw_iwos_odometer/ImuOdometerConfig.h>
+#include <tuw_iwos_odometer/side.h>
+#include <dynamic_reconfigure/server.h>
+
+namespace tuw_iwos_odometer
+{
+class ImuAccelerationOdometer
+{
+public:
+  ImuAccelerationOdometer();
+  ~ImuAccelerationOdometer() = default;
+  bool update(const sensor_msgs::Imu& imu, const std::shared_ptr<ros::Duration>& duration = nullptr);
+  std::shared_ptr<geometry_msgs::TransformStamped> get_transform();
+  std::shared_ptr<nav_msgs::Odometry> get_message();
+  cv::Vec<double, 3> get_velocity();
+  tuw::Pose2D get_pose();
+protected:
+
+  static double integrate(double f, double c, double dt, double steps);
+
+  ImuOdometerConfig config_;
+  dynamic_reconfigure::Server<ImuOdometerConfig> reconfigure_server_;
+  dynamic_reconfigure::Server<ImuOdometerConfig>::CallbackType callback_type_;
+
+  geometry_msgs::Quaternion quaternion_;
+  std::shared_ptr<nav_msgs::Odometry> message_;
+  std::shared_ptr<geometry_msgs::TransformStamped> transform_;
+
+  ros::Time this_time_;
+  ros::Time last_time_;
+  ros::Duration duration_;
+
+  cv::Vec<double, 3> angular_velocity_{0.0, 0.0, 0.0};
+  cv::Vec<double, 3> linear_acceleration_{0.0, 0.0, 0.0};
+  cv::Vec<double, 3> velocity_{0.0, 0.0, 0.0};
+  tuw::Pose2D pose_{0.0, 0.0, 0.0};
+};
+}
+
+#endif //DIP_WS_IMU_ODOMETER_H
---
+++ tuw_iwos_odometer/src/tuw_iwos_odometer/imu_odometer.cpp
@@ -0,0 +1,106 @@
+// Copyright 2023 Eugen Kaltenegger
+
+#include "tuw_iwos_odometer/imu_odometer.h"
+
+using tuw_iwos_odometer::ImuAccelerationOdometer;
+
+ImuAccelerationOdometer::ImuAccelerationOdometer()
+{
+  this->message_ = std::make_shared<nav_msgs::Odometry>();
+  this->transform_ = std::make_shared<geometry_msgs::TransformStamped>();
+
+  this->message_ = std::make_shared<nav_msgs::Odometry>();
+  this->message_->header.frame_id = "odom";
+  this->message_->child_frame_id = "base_link";
+  this->message_->pose.pose.position.x = 0.0;
+  this->message_->pose.pose.position.y = 0.0;
+  this->message_->pose.pose.position.z = 0.0;
+  this->message_->pose.pose.orientation = this->quaternion_;
+  this->message_->twist.twist.linear.x = 0.0;
+  this->message_->twist.twist.linear.y = 0.0;
+  this->message_->twist.twist.linear.z = 0.0;
+  this->message_->twist.twist.angular.x = 0.0;
+  this->message_->twist.twist.angular.y = 0.0;
+  this->message_->twist.twist.angular.z = 0.0;
+
+  this->transform_ = std::make_shared<geometry_msgs::TransformStamped>();
+  this->transform_->header.frame_id = "odom";
+  this->transform_->child_frame_id = "base_link";
+  this->transform_->transform.translation.x = 0.0;
+  this->transform_->transform.translation.y = 0.0;
+  this->transform_->transform.translation.z = 0.0;
+  this->transform_->transform.rotation = this->quaternion_;
+}
+
+bool ImuAccelerationOdometer::update(const sensor_msgs::Imu& imu, const std::shared_ptr<ros::Duration> &duration)
+{
+  if (duration == nullptr)
+  {
+    this->this_time_ = ros::Time::now();
+    this->duration_ = this->this_time_ - this->last_time_;
+  } else
+  {
+    this->duration_ = *duration;
+  }
+
+  this->linear_acceleration_[0] = imu.linear_acceleration.x;
+  this->linear_acceleration_[1] = imu.linear_acceleration.y;
+  this->linear_acceleration_[2] = imu.linear_acceleration.z;
+
+  this->angular_velocity_[0] = imu.angular_velocity.x;
+  this->angular_velocity_[1] = imu.angular_velocity.y;
+  this->angular_velocity_[2] = imu.angular_velocity.z;
+
+  double dt = this->duration_.toSec();
+
+  double ax = this->linear_acceleration_[0];
+  double ay = this->linear_acceleration_[1];
+
+  double wz = this->angular_velocity_[2];
+
+  double vx = integrate(ax, this->velocity_[0], dt, this->config_.acceleration_integration_iterations);
+  double vy = integrate(ay, this->velocity_[1], dt, this->config_.acceleration_integration_iterations);
+
+  this->velocity_[0] = vx;
+  this->velocity_[1] = vy;
+  this->velocity_[2] = wz;
+
+  double x = integrate(vx, this->pose_.x(), dt, this->config_.velocity_integration_iterations);
+  double y = integrate(vy, this->pose_.y(), dt, this->config_.velocity_integration_iterations);
+  double theta = integrate(wz, this->pose_.theta(), dt, this->config_.velocity_integration_iterations);
+
+  this->pose_ = tuw::Pose2D(x, y, theta);
+
+  return true;
+}
+
+std::shared_ptr<geometry_msgs::TransformStamped> ImuAccelerationOdometer::get_transform()
+{
+  return this->transform_;
+}
+
+std::shared_ptr<nav_msgs::Odometry> ImuAccelerationOdometer::get_message()
+{
+  return this->message_;
+}
+
+double ImuAccelerationOdometer::integrate(double f, double c, double dt, double steps)
+{
+  double x = 0.0;
+  double dt_step = dt / steps;
+  for (int i = 0; i < steps; i++)
+  {
+     x += f * dt_step;
+  }
+  return x + c;
+}
+
+cv::Vec<double, 3> ImuAccelerationOdometer::get_velocity()
+{
+  return this->velocity_;
+}
+
+tuw::Pose2D ImuAccelerationOdometer::get_pose()
+{
+  return this->pose_;
+}
