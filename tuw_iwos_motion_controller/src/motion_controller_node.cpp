759caa763eba709224bed0fad001c80c19a94df1
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2023-03-08 11:35:40 +0100

    finished draft for motion controller

--- tuw_iwos_motion_controller/src/motion_controller_node.cpp
+++ tuw_iwos_motion_controller/src/motion_controller_node.cpp
@@ -1,12 +1,21 @@
 // Copyright 2023 Eugen Kaltenegger
 
 #include "../include/motion_controller_node.h"
+#include <tuw_iwos_motion_controller/motion_controller.h>
 
 #include <ros/ros.h>
+
+using tuw_iwos_motion_controller::MotionController;
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "TUW_IWOS_MOTION_CONTROLLER");
+
+  ros::NodeHandle node_handle;
+
+  MotionController motion_controller(node_handle, 0.5);
+  motion_controller.run();
+
 
   ROS_INFO("HELLO WORLD");
 }
