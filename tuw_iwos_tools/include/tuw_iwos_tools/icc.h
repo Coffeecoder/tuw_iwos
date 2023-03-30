8e2d615f14808f3ae4b7b0e67b1834b5f34560b0
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2023-03-30 12:35:42 +0200

    initial commit for tuw_iwos_tools package

---
+++ tuw_iwos_tools/include/tuw_iwos_tools/icc.h
@@ -0,0 +1,13 @@
+// Copyright 2023 Eugen Kaltenegger
+
+#ifndef DIP_WS_ICC_H
+#define DIP_WS_ICC_H
+
+
+class icc
+{
+
+};
+
+
+#endif //DIP_WS_ICC_H
---
+++ tuw_iwos_tools/src/tuw_iwos_tools/icc.cpp
@@ -0,0 +1,3 @@
+// Copyright 2023 Eugen Kaltenegger
+
+#include "../../include/tuw_iwos_tools/icc.h"
