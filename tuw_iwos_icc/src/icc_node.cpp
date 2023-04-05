be61ac1cee33a8349011bb8251b89044153c08eb
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2023-04-05 11:29:15 +0200

    remove icc package

--- tuw_iwos_icc/src/icc_node.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 Eugen Kaltenegger
-
-#include "../include/icc_node.h"
-
-int main(int argc, char **argv)
-{
-  ROS_INFO("HELLO WORLD");
-}
--- tuw_iwos_icc/src/tuw_iwos_icc/icc_calculator.cpp
+++
@@ -1,3 +0,0 @@
-// Copyright 2023 Eugen Kaltenegger
-
-#include "../../include/tuw_iwos_icc/icc_calculator.h"
